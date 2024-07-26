36c4af72f8fff15b52cb316c29fd9f718228a37c
sivraamkrishnankv <sivraamkrishnankv@gmail.com>
2024-07-27 01:36:20 +0530

    files of face recognition

---
+++ dataset-training-using-cam.py
@@ -0,0 +1,41 @@
+import cv2,os
+haar_file='haarcascade_frontalface_default.xml'
+dataset = 'datasets'
+sub_data= 'sivraamkrishnan'
+
+path = os.path.join(dataset, sub_data)     #datasets/sub_data
+if not os.path.isdir(path):
+    os.mkdir(path)
+(width, height) = (130,100)
+
+face_cascade = cv2.CascadeClassifier(haar_file)
+webcam=cv2.VideoCapture(0)
+
+count=1
+while count< 50:
+    print(count)
+    (_, im) = webcam.read()
+    gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray,1.3,4)
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
+        face = gray[y:y+h,x:x+w]    #crop the face part alone
+        face_resize = cv2.resize ( face, (width, height))   #store the cropped face by resize in same size
+        cv2.imwrite('%s/%s.png' % (path,count), face_resize)
+    count+=1
+
+    cv2.imshow('openCV',im)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+webcam.release()
+cv2.destroyAllWindows()
+    
+
+
+
+
+
+    
